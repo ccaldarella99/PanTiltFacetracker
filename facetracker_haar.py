d4e2dee9632d3ad5833afdd6ef2c79c4290f9d6a
Christopher Caldarella <christopher.caldarella@gmail.com>
2021-07-12 16:12:23 -0400

    updated facetracker_haar.py for modern cv2 libraries

--- facetracker_haar.py
+++ facetracker_haar.py
@@ -4,114 +4,361 @@
 Modified from code posted here: http://forums.pimoroni.com/t/pan-tilt-hat-repo/3402/11
 """
 import numpy as np
-import cv2.cv as cv
+import cv2
+# import cv2.cv as cv
 import os
-from pantilthat import *
+import sys
+import time
+import pantilthat as pth
+# from pantilthat import *
+import argparse
 
-os.system('sudo modprobe bcm2835-v4l2')
 
-cascade = cv.Load('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
-#cascade = cv.Load('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+# os.system('sudo modprobe bcm2835-v4l2')
 
-cam_pan = 90
-cam_tilt = 45
+
+# Input Args/Switches
+ap = argparse.ArgumentParser()
+ap.add_argument("-bbb", "--show_blue_border_box", action="store_true", default=0, help="Draws a (blue) border boundary box which can dictate camera movement. Default: Show box")
+ap.add_argument("-p", "--show_person_box", action="store_true", default=0, help="Draws a (green) box around the person. Default: Show box")
+ap.add_argument("-m", "--still_camera", action="store_true", default=0, help="Keep camera Still when enabled (1); otherwise the camera moves (0). Default: Camera moves")
+ap.add_argument("-c", "--is_cascade", action="store_true", default=0, help="Whether to use LBP Cascade (1) or Haar Cascade (0). Default: Haar Cascade (0)")
+args = ap.parse_args()
+
+
+# GLOBALS
+key_letter = 27
+show_text = False
+is_yolo_face = False
+show_boundary_box = args.show_blue_border_box
+show_person_box = args.show_person_box
+is_camera_still = args.still_camera
+is_cascade = args.is_cascade
+shrink_person_box = False
+show_onscreen_help = False
+# Frame dimensions vars
+FRAME_W = FRAME_H = 0
+x = y = w = h = 0
+# Boundary Box vars
+w_min = w_max = h_min = h_max = 0
+
+
+def man_move_camera(key_press):
+    """Take keystrokes to dictate camera movemement.
+
+    # Argument:
+        key_press: takes in one letter for movement
+                    (same as gaming controls, no inversion):
+                        w: Up
+                        a: Left
+                        s: Down
+                        d: Right
+
+    # Returns
+        None
+    """
+    cam_pan = pth.get_pan()
+    cam_tilt = pth.get_tilt()
+    move_x = 0
+    move_y = 0
+
+    if(key_press.lower() == 'a'):
+        move_x = -2
+    elif(key_press.lower() == 'd'):
+        move_x = 2
+    elif(key_press.lower() == 's'):
+        move_y = -1
+    elif(key_press.lower() == 'w'):
+        move_y = 1
+
+    if((cam_pan + move_x < 90) & (cam_pan - move_x > -90)):
+        cam_pan += move_x
+        pth.pan(int(cam_pan))
+        time.sleep(0.005)
+    else:
+        print(f'MAX PAN - cannot move:  {cam_pan + move_x}')
+
+    if((cam_tilt + move_y < 90) & (cam_tilt - move_y > -90)):
+        cam_tilt -= move_y
+        pth.tilt(int(cam_tilt))
+        time.sleep(0.005)
+    else:
+        print(f'MAX TILT - cannot move:  {cam_tilt + move_y}')
+    return
+
+
+def move_camera(x, y, w, h, invert=1):
+    """Takes in object tracking coordinates and
+        moves camera to try to "center" the subject.
+
+    # Argument:
+        x: coordinate on the x axis where subject is detected
+        y: coordinate on the y axis where subject is detected
+        w: width of object detected on screen
+        h: height of object detected on screen
+
+    # Returns
+        None
+    """
+    if(is_camera_still):
+        return
+    if(shrink_person_box):
+        #for camera tracking only
+        # shrink height and width by half
+        w = w//2
+        h = h//2
+        # center box by adding a quarter of w/h to x/y
+        x += w//2
+        y += h//2
+
+    cam_pan = pth.get_pan()
+    cam_tilt = pth.get_tilt()
+    move_x = 2 * invert
+    move_y = 1 * invert
+    yolo_offset = 0 if is_cascade else (h_min * -0.75)
+
+    if(((x + w)*0.95 > w_max) & (x*0.95 < w_min)):
+        # If both subject borders take up 95% or
+        # more of the boundary box, do nothing
+        pass
+    elif(w > (w_max - w_min)*0.95):
+        # If subject border-length take up 95% (not centered)
+        # or more of the boundary box, correct movement by aligning centers
+        if(x + w/2 > (FRAME_W + w_min)/2):
+            cam_pan += move_x
+            pth.pan(int(cam_pan))
+        elif(x - w/2 < (FRAME_W - w_min)/2):
+            cam_pan -= move_x
+            pth.pan(int(cam_pan))
+    elif((cam_pan + move_x < 90) & (cam_pan - move_x > -90)):
+        if(x + w > w_max):
+            cam_pan += move_x
+            pth.pan(int(cam_pan))
+        elif(x < w_min):
+            cam_pan -= move_x
+            pth.pan(int(cam_pan))
+    else:
+        print(f'MAX PAN - cannot move:  {cam_pan + move_x}')
+
+    if(((y + h)*0.95 > h_max) & (y*0.95 < h_min)):
+        # If both subject borders take up 95% or
+        # more of the boundary box, do nothing
+        pass
+    elif(h > (h_max - h_min)*0.95):
+        # If subject border-length take up 95% (not centered)
+        # or more of the boundary box, correct movement by aligning centers
+        if(y + h/2 > (FRAME_H + h_min)/2):
+            cam_tilt += move_y
+            pth.tilt(int(cam_tilt))
+        elif(y - h/2 < (FRAME_H - h_min)/2):
+            cam_tilt -= move_y
+            pth.tilt(int(cam_tilt))
+    elif((cam_tilt + move_y < 90) & (cam_tilt - move_y > -90)):
+        if(y + h > h_max):
+            cam_tilt += move_y
+            pth.tilt(int(cam_tilt))
+        elif(y < h_min + yolo_offset):
+            cam_tilt -= move_y
+            pth.tilt(int(cam_tilt))
+    else:
+        print(f'MAX TILT - cannot move:  {cam_tilt + move_y}')
+    return
+
+
+def reset_camera_position():
+    """Resets Camera position.
+
+    # Argument:
+        None
+
+    # Returns:
+        None
+    """
+    pth.pan(0)
+    pth.tilt(-20)
+    time.sleep(2)
+
+
+# Deprecated
+# cascade = cv2.Load('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
+# cascade = cv2.Load('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+
+cascade_path = '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml' 
+if(is_cascade):
+    cascade_path = '/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml'
+face_cascade = cv2.CascadeClassifier(cascade_path)
+
+# Use function instead:
+# cam_pan = 90
+# cam_tilt = 45
 
 # Turn the camera to the default position
-pan(cam_pan-90)
-tilt(cam_tilt-90)
-light_mode(WS2812)
+# pan(cam_pan-90)
+# tilt(cam_tilt-90)
 
-def lights(r,g,b,w):
-    for x in range(18):
-        set_pixel_rgbw(x,r if x in [3,4] else 0,g if x in [3,4] else 0,b,w if x in [0,1,6,7] else 0)
-    show()
+## Lights removed at the moment ##
+# light_mode(WS2812)
 
-lights(0,0,0,50)
+# def lights(r,g,b,w):
+#     for x in range(18):
+#         set_pixel_rgbw(x,r if x in [3,4] else 0,g if x in [3,4] else 0,b,w if x in [0,1,6,7] else 0)
+#     show()
+
+# lights(0,0,0,50)
+
+reset_camera_position()
+
 
 min_size = (15, 15)
 image_scale = 5
 haar_scale = 1.2
 min_neighbors = 2
-haar_flags = cv.CV_HAAR_DO_CANNY_PRUNING
+haar_flags = 0
+# Deprectated
+# haar_flags = cv2.HAAR_DO_CANNY_PRUNING
 
-cap = cv.CreateCameraCapture(0)
-cv.NamedWindow("Tracker", 1)
+
+cap = cv2.VideoCapture(0)
+# Deprecated
+# cap = cv2.CreateCameraCapture(0)
+# cv2.NamedWindow("Tracker", 1)
  
-if cap:
-    frame_copy = None
+# if cap:
+#     frame_copy = None
     
+# Set placement vars
+FRAME_W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # width
+FRAME_H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # height
+# For BOUNDARY BOX
+w_min = int((FRAME_W)/6)
+w_max = int((FRAME_W) - w_min)
+h_min = int((FRAME_H)/5)
+h_max = int((FRAME_H) - h_min)
+
+
 while(True):
+    ret, frame = cap.read()
+    if(not ret):
+        print("Error getting image")
+        continue
+    # Deprecated 
     # Capture frame-by-frame
-    frame = cv.QueryFrame(cap)
-    if not frame:
-        cv.WaitKey(0)
-        break
-    if not frame_copy:
-        frame_copy = cv.CreateImage((frame.width,frame.height),
-                                            cv.IPL_DEPTH_8U, frame.nChannels)
-    if frame.origin == cv.IPL_ORIGIN_TL:
-        cv.Flip(frame, frame, -1)
+    # frame = cv2.QueryFrame(cap)
+    # if not frame:
+    #     cv2.WaitKey(0)
+    #     break
+    # if not frame_copy:
+    #     frame_copy = cv2.CreateImage((frame.width,frame.height), cv2.IPL_DEPTH_8U, frame.nChannels)
+    # if frame.origin == cv2.IPL_ORIGIN_TL:
+    #     cv2.Flip(frame, frame, -1)
+    frame = cv2.flip(frame, 0)
    
     # Our operations on the frame come here
-    gray = cv.CreateImage((frame.width,frame.height), 8, 1)
-    small_img = cv.CreateImage((cv.Round(frame.width / image_scale),
-                   cv.Round (frame.height / image_scale)), 8, 1)
+    # gray = cv2.CreateImage((frame.width,frame.height), 8, 1)
+    # small_img = cv2.CreateImage((cv2.Round(frame.width / image_scale),
+    #                cv2.Round (frame.height / image_scale)), 8, 1)
  
     # convert color input image to grayscale
-    cv.CvtColor(frame, gray, cv.CV_BGR2GRAY)
+    # cv2.CvtColor(frame, gray, cv2.BGR2GRAY)
+    cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  
     # scale input image for faster processing
-    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+    # cv2.Resize(gray, small_img, cv2.INTER_LINEAR)
  
-    cv.EqualizeHist(small_img, small_img)
+    # cv2.EqualizeHist(small_img, small_img)
 
     midFace = None
  
-    if(cascade):
-        t = cv.GetTickCount()
+    if(True):
+        # t = cv2.GetTickCount()
         # HaarDetectObjects takes 0.02s
-        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
-                                     haar_scale, min_neighbors, haar_flags, min_size)
-        t = cv.GetTickCount() - t
-        if faces:
-            lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
+        # faces = cv2.HaarDetectObjects(small_img, cascade, cv2.CreateMemStorage(0), haar_scale, min_neighbors, haar_flags, min_size)
+        faces = face_cascade.detectMultiScale(frame, 1.1, 3)
+        # t = cv2.GetTickCount() - t
+        if(True):
+        # if faces:
+            # lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
 
-            for ((x, y, w, h), n) in faces:
-                # the input to cv.HaarDetectObjects was resized, so scale the
-                # bounding box of each face and convert it to two CvPoints
-                pt1 = (int(x * image_scale), int(y * image_scale))
-                pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-                cv.Rectangle(frame, pt1, pt2, cv.RGB(100, 220, 255), 1, 8, 0)
-                # get the xy corner co-ords, calc the midFace location
-                x1 = pt1[0]
-                x2 = pt2[0]
-                y1 = pt1[1]
-                y2 = pt2[1]
+            # for ((x, y, w, h), n) in faces:
+            for (x, y, w, h) in faces:
+                # # the input to cv2.HaarDetectObjects was resized, so scale the
+                # # bounding box of each face and convert it to two CvPoints
+                # pt1 = (int(x * image_scale), int(y * image_scale))
+                # pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
+                # cv2.Rectangle(frame, pt1, pt2, cv2.RGB(100, 220, 255), 1, 8, 0)
+                # # get the xy corner co-ords, calc the midFace location
+                # x1 = pt1[0]
+                # x2 = pt2[0]
+                # y1 = pt1[1]
+                # y2 = pt2[1]
 
-                midFaceX = x1+((x2-x1)/2)
-                midFaceY = y1+((y2-y1)/2)
-                midFace = (midFaceX, midFaceY)
+                # midFaceX = x1+((x2-x1)/2)
+                # midFaceY = y1+((y2-y1)/2)
+                # midFace = (midFaceX, midFaceY)
 
-                offsetX = midFaceX / float(frame.width/2)
-                offsetY = midFaceY / float(frame.height/2)
-                offsetX -= 1
-                offsetY -= 1
+                # offsetX = midFaceX / float(frame.width/2)
+                # offsetY = midFaceY / float(frame.height/2)
+                # offsetX -= 1
+                # offsetY -= 1
 
-                cam_pan -= (offsetX * 5)
-                cam_tilt += (offsetY * 5)
-                cam_pan = max(0,min(180,cam_pan))
-                cam_tilt = max(0,min(180,cam_tilt))
+                # cam_pan -= (offsetX * 5)
+                # cam_tilt += (offsetY * 5)
+                # cam_pan = max(0,min(180,cam_pan))
+                # cam_tilt = max(0,min(180,cam_tilt))
 
-                print(offsetX, offsetY, midFace, cam_pan, cam_tilt, frame.width, frame.height)
+                # print(offsetX, offsetY, midFace, cam_pan, cam_tilt, frame.width, frame.height)
+                
+                
+                if(show_person_box):
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.putText(frame, 'Haar Cascade', (x, y - 6),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.6, (0, 255, 0), 1,
+                                cv2.LINE_AA)
 
-                pan(int(cam_pan-90))
-                tilt(int(cam_tilt-90))
+                # pth.pan(int(cam_pan-90))
+                # pth.tilt(int(cam_tilt-90))
+                if(not is_camera_still):
+                    move_camera(x, y, w, h)
                 break
+    
+    if(show_boundary_box):
+        cv2.rectangle(frame, (w_min, h_min), (w_max, h_max), (255, 0, 0), 2)
                 
     # Display the resulting frame
-    cv.ShowImage('Tracker',frame)
-    if cv.WaitKey(1) & 0xFF == ord('q'):
+    # cv2.ShowImage('Tracker',frame)
+    cv2.imshow('Tracker', frame)
+    
+    # if cv2.WaitKey(1) & 0xFF == ord('q'):
+    #     break
+    key_stroke = cv2.waitKey(1)
+    key_letter = key_stroke
+    if key_stroke & 0xFF == ord('q'):
         break
+    elif key_stroke & 0xFF == 27:
+        break
+    elif key_stroke & 0xFF == ord('b'):
+        show_boundary_box = not show_boundary_box
+    elif key_stroke & 0xFF == ord('p'):
+        show_person_box = not show_person_box
+    elif key_stroke & 0xFF == ord('m'):
+        is_camera_still = (not is_camera_still)
+    elif key_stroke & 0xFF == ord('c'):
+        is_cascade = not is_cascade
+        
+    if key_stroke & 0xFF == ord('r'):
+        reset_camera_position()
+    elif key_stroke & 0xFF == ord('w'):
+        man_move_camera('w')
+    elif key_stroke & 0xFF == ord('a'):
+        man_move_camera('a')
+    elif key_stroke & 0xFF == ord('s'):
+        man_move_camera('s')
+    elif key_stroke & 0xFF == ord('d'):
+        man_move_camera('d')
+
 
 # When everything done, release the capture
-cv.DestroyWindow("Tracker")
+# cv2.DestroyWindow("Tracker")
+cap.release()
+cv2.destroyAllWindows()
